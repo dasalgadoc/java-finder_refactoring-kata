295e49eff692ee19978f29349ab8356ee139707c
Diego Salgado <diego.salgado@mercadolibre.com.co>
2022-10-22 23:15:16 -0500

    Remove unused on Person class

--- src/main/java/algorithm/Person.java
+++ src/main/java/algorithm/Person.java
@@ -11,19 +11,8 @@ public class Person {
         this.birthDate = birthDate;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Date getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
 }
